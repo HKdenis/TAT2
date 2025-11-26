369254bd707a26c2ad8ee74f2e015a9712b8c8d9
HKdenis <kayanjadenish@gmail.com>
2025-11-26 03:01:36 +0300

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -6,8 +6,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2 import service_account
 import os
 
-st.page_link("pages/Data_entry.py", label="First Page", icon="🏠")
-st.page_link("pages/Mentorship_Records.py", label="Second Page", icon="📄")
+st.page_link("pages/Data_entry.py")
+st.page_link("pages/Mentorship_Records.py")
 
 st.set_page_config(page_title="Technical assistance tracker",
     page_icon="🧊",
@@ -73,6 +73,7 @@ st.line_chart(monthly_counts)
 
 
 st.markdown("Developed by EMTCT © Nov 2025")
+
 
 
 
