72e7403bc426904ebaa88690d32492631d893698
HKdenis <kayanjadenish@gmail.com>
2025-12-20 00:15:48 +0300

    Refactor Homepage.py for improved structure and clarity

--- Homepage.py
+++ Homepage.py
@@ -6,9 +6,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2 import service_account
 import os
 import datetime
+from st_pages import Page, show_pages, add_page_title
 
+# Optional -- adds the title and icon to the current page
+add_page_title(Technical Assistance Tool)
 
-set_page.config(
+# Specify what pages should be shown in the sidebar, and what their titles 
+# and icons should be
+Show_pages(
     [
         Page("Homepage.py", "Technical Assistance Tool", "🏠"),
         Page("Data_entry.py", "Activity Report", ":books:"),
@@ -71,6 +76,7 @@ st.line_chart(monthly_counts)
 
 
 st.markdown("Developed by EMTCT © Nov 2025")
+
 
 
 
