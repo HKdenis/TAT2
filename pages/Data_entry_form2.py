edbfa230903167a2bed5014d2749a4c57a24dc2b
HKdenis <kayanjadenish@gmail.com>
2025-12-19 14:48:01 +0300

    Set page config for Data entry form2

--- pages/Data_entry_form2.py
+++ pages/Data_entry_form2.py
@@ -20,7 +20,7 @@ client = gspread.authorize(creds)
 # Open the Google Sheet
 sheet = client.open("Mentorship tool").worksheet("TA Data Entry form2")
 
-st.header("Other TA ACTIVITIES", page_icon="Contract Edit")
+st.set_page_config(page_title="Data entry form2", page_icon=":material/Contract Edit:")
 st.markdown(">Please fill in the form below to submit Other activities!")
 
 
