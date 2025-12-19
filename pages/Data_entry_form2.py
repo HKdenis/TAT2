f7e384f2e08b4be3c769eee794708844c41ac838
HKdenis <kayanjadenish@gmail.com>
2025-12-19 14:41:11 +0300

    Update page icon for Data Entry form

--- pages/Data_entry_form2.py
+++ pages/Data_entry_form2.py
@@ -20,7 +20,7 @@ client = gspread.authorize(creds)
 # Open the Google Sheet
 sheet = client.open("Mentorship tool").worksheet("TA Data Entry form2")
 
-st.set_page_config(page_title="Entry Fotm_Other Activities", page_icon=" ")
+st.set_page_config(page_title="Entry Fotm_Other Activities", page_icon="Contract Edit")
 st.markdown(">Please fill in the form below to submit Other activities!")
 
 
