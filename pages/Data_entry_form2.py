3986e25c30ea7e4ef662eaa01bbb8dcd8b4096b0
HKdenis <kayanjadenish@gmail.com>
2025-12-19 14:39:01 +0300

    Update page title for Data Entry Form

--- pages/Data_entry_form2.py
+++ pages/Data_entry_form2.py
@@ -19,7 +19,8 @@ client = gspread.authorize(creds)
 
 # Open the Google Sheet
 sheet = client.open("Mentorship tool").worksheet("TA Data Entry form2")
-st.set_page_config(page_title="Material Icons Example", page_icon="Contract Edit")
+
+st.set_page_config(page_title="Entry Fotm_Other Activities", page_icon="Contract Edit")
 st.markdown(">Please fill in the form below to submit Other activities!")
 
 
